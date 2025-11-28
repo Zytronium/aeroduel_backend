2b0ffd85a5be0a50a9693a42808e7f3c43da2b70
Zytronium <dastelljes@outlook.com>
2025-11-28 14:47:56 -0600

    remove empty type files and keep everything in one unified types file

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,38 @@
-export * from './match';
-// export * from './plane';
-// export * from './api';
+export interface MatchState {
+  matchId: string;
+  gamePin: string;
+  status: "waiting" | "active" | "ended";
+  createdAt: Date;
+  matchType: "timed"; // | "scored" | "lives"; // (Stretch goal match types)
+  duration: number;      // match duration in seconds
+  matchPlanes: string[]; // planeIds of planes that have joined the match
+  maxPlayers: number;
+  serverUrl: string;
+  wsUrl: string;
+  events: Event[];
+}
+
+export interface Plane {
+  /* Registration info */
+  esp32Ip?: string;
+  planeId: string;
+  userId: string;
+  playerName?: string;
+  registeredAt: Date;
+
+  /* Match info */
+  hits?: number;
+  hitsTaken?: number;
+
+  /* Misc booleans */
+  isOnline: boolean;
+  isJoined: boolean;
+  isDisqualified: boolean;
+}
+
+export interface Event {
+  type: "join" | "leave" | "hit" | "disqualify";
+  planeId: string;
+  targetId?: string; // for hit events
+  timestamp: Date;
+}
