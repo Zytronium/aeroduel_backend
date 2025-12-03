8b1a2bc49c74dedf85bb131ea9ade88194f4c08b
Zytronium <dastelljes@outlook.com>
2025-12-03 12:47:18 -0600

    Fix server tokens not working in prod

--- src/app/getAuth.ts
+++ src/app/getAuth.ts
@@ -2,13 +2,19 @@
 export default async function getServerToken() {
   if (typeof window !== 'undefined' && (window as any).electron) {
     // We are in Electron
-    if ((window as any).electron.isDev()) {
+    const isDev =
+      typeof (window as any).electron.isDev === "function"
+        ? await (window as any).electron.isDev()
+        : false;
+
+    if (isDev) {
       // in dev mode (from `npm run dev`), the server token is always undefined.
       // This is a bug, but as it only occurs in dev mode, is not a security risk,
       // and therefore will not be fixed.
       return undefined;
     }
+
     return await (window as any).electron.getServerToken();
   }
-  return undefined; // We are on an external device (mobile phone, browser, etc.) or in dev mode
+  return null; // We are on an external device (mobile phone, browser, etc.)
 }
